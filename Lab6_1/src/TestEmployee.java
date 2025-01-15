c0f3f6cc062b876555428a5f6ba987e1469ecd15
taithut-psk <2413110798@tni.ac.th>
2025-01-15 10:11:56 +0700

    Lab6

---
+++ Lab6_1/src/TestEmployee.java
@@ -0,0 +1,14 @@
+
+public class TestEmployee {
+	public static void main(String[] args) {
+
+		Employee emp1 = new Employee();
+		Employee emp2 = new Employee();
+
+		emp1.setEmployeeDetails("Natcha", 45, 20.0);
+		emp2.setEmployeeDetails("Attakorn", 38, 25.0);
+
+		System.out.println("Employee 1 Details");
+		//emp1.setEmployeeDetails();
+	}
+}
